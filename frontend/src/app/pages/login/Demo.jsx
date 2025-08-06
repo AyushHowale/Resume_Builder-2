ecae3592f3b0af62c1bd7bf1adb826ee1ee905fa
AyushHowale <118345207+AyushHowale@users.noreply.github.com>
2025-08-06 12:36:57 +0530

    updated using fork

--- frontend/src/app/pages/login/Demo.jsx
+++ frontend/src/app/pages/login/Demo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Demo() {
   return (
     <div className='text-center text-red-600'>
-      <h1>Hello</h1>
+      <h1 className='text-green-500'>Hello</h1>
       <h2>bye</h2>
     </div>
   )
